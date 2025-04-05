05641450d8638aea0dacde12336de0e7e75049e9
himanshuIntoAi <himanshu@theai.cloud>
2025-04-05 13:45:10 +0530

    created slice for cart

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Building A Redux Shopping Cart</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/slice/cartSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    items : [],
+    totalQuantity : 0 ,
+    totalPrice : 0
+}
+
+
+const cartSlice = createSlice({
+    name :"cart",
+    initialState , 
+    reducers : {
+        addToCart : (state , action) =>{
+            const item = state.items.find((item)=>{item.id === action.payload.id})
+
+            if(item){
+                item.quantity += 1
+            }else{
+                state.items.push({...action.payload , quantity : 1 })
+            }
+
+            state.totalQuantity += 1
+            state.totalPrice += action.payload.price
+        } , 
+        removeFromCart : (state,action)=>{
+            const item = state.items.find((item)=>{item.id == action.payload.id})
+            if(item){
+                state.totalQuantity -= item.quantity;
+                state.totalPrice -= item.price * item.quantity
+                state.items = state.items.filter((item)=>{item.id !== action.payload.id})
+            }
+        } , 
+        clearCart :(state)=>{
+            state.item = [],
+            state.totalQuantity = 0 ,
+            state.totalPrice = 0
+        }
+    }
+})
